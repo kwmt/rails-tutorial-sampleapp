d25c310af413a6a756555c7f1f4e36145da5c435
Yasutaka.Kawamoto <yasutaka.kawamoto@appbrew.io>
2022-06-17 16:27:27 +0900

    今後自動的にslimファイルを作成されるようにするための設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.generators.template_engine = :slim # 今後自動的にslimファイルを作成されるようにするため。
 
     # Configuration for the application, engines, and railties goes here.
     #
